c94d1b585df32e1994cb154c77b06d2c9ce1acd0
mandeuk <lih2677@naver.com>
2018-01-21 22:38:42 +0900

    join Twitch IRC and Read chat in Channel
    Success to connect Twitch IRC Server but Sometimes cannot connect Channel in IRC Server
    
    function
    : join IRC Server - need OAuth password  it can get https://twitchapps.com/tmi/
    : read chat message in Specific Channel you type in "channelName"

---
+++ TwitchScript/TwitchChat.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.ComponentModel;
+using System.Net.Sockets;
+using System.IO;
+
+public class TwitchChat : MonoBehaviour {
+
+    private TcpClient twitchClient;
+    private StreamReader reader;
+    private StreamWriter writer;
+
+    public string username, password, channelName; //Get the password from https://twitchapps.com/tmi
+
+	// Use this for initialization
+	void Start () {
+        Connect();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!twitchClient.Connected)
+        {
+            //만약 트위치 Client가 irc서버에 접속하지 못했을 경우 다시 접속 시도
+            Connect();
+        }
+
+        ReadChat();
+    }
+
+    private void Connect()
+    {
+        twitchClient = new TcpClient("irc.chat.twitch.tv", 6667);
+        reader = new StreamReader(twitchClient.GetStream());
+        writer = new StreamWriter(twitchClient.GetStream());
+
+        writer.WriteLine("PASS " + password);
+        writer.WriteLine("NICK " + username);
+        writer.WriteLine("USER " + username + " 8 * :" + username);
+        writer.WriteLine("JOIN #" + channelName);
+        writer.Flush();//Flush() = 해당 writer의 버퍼를 clear하는 기능
+    }
+
+    private void ReadChat()
+    {
+        if(twitchClient.Available > 0)
+        {
+            var message = reader.ReadLine();
+
+            print(message);
+
+            if(message.Contains("PRIVMSG"))
+            {
+                var splitPoint = message.IndexOf("!", 1);
+                var chatName = message.Substring(0, splitPoint);
+                chatName = chatName.Substring(1);
+
+                splitPoint = message.IndexOf(":", 1);
+                message = message.Substring(splitPoint + 1);
+                print(String.Format("{0}: {1}", chatName, message));
+            }
+        }
+    }
+}
