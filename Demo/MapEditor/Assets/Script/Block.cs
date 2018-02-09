527ddbf6442acbae2e6522702e0af151b0b734a5
psshoon <psshoon123@naver.com>
2018-02-09 23:08:25 +0900

    맵 제작기능 있는 에디터
    미완.
    클릭으로 맵의 타일 타입을 정해줌.

---
+++ Demo/MapEditor/Assets/Script/Block.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Block : MonoBehaviour {
+    public bool selectBlockClone;
+    public int blockNum;
+
+    Block blockClones;
+
+    // Use this for initialization
+    void Start () {
+        blockNum = 1;
+        selectBlockClone = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (selectBlockClone)
+        {
+            SelectObjectBlock();
+        }
+
+        else
+        {
+            transform.GetComponent<MeshRenderer>().material.color = Color.white;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.GetComponent<Block>().blockNum = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.GetComponent<Block>().blockNum = 2;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            this.GetComponent<Block>().blockNum = 3;
+        }
+    }
+
+    public void SelectObjectBlock()
+    {
+        if (blockNum == 1)
+            transform.GetComponent<MeshRenderer>().material.color = Color.white;
+
+        if (blockNum == 2)
+            transform.GetComponent<MeshRenderer>().material.color = Color.red;
+
+        if (blockNum == 3)
+            transform.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    public void Block1Con()
+    {
+        blockNum = 1;
+        transform.GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+
+    public void Block2Con()
+    {
+        blockNum = 2;
+        transform.GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    public void Block3Con()
+    {
+        blockNum = 3;
+        transform.GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+}
---
+++ Demo/MapEditor/Assets/Script/BlockSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawn : MonoBehaviour {
+    public GameObject blockPos;
+    Block blockClones;
+
+    // Use this for initialization
+    void Start () {
+        List<Block> blockList = new List<Block>();
+
+        for (int i = 0; i < 20; i++)
+            for (int j = 0; j < 20; j++)
+            {
+                blockClones = Instantiate(blockPos, new Vector3(1.2f * i, 1.2f * j, 0), blockPos.transform.rotation).GetComponent<Block>();
+                blockList.Add(blockClones);
+            }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
