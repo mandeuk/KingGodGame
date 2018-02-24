3b96f3d7131c7f923808c2e690044990ebaa6cd7
psshoon <psshoon123@naver.com>
2018-02-24 20:59:51 +0900

    맵툴 제작 세이브기능 미포함
    블럭 셀렉트 가능
    블럭마다 num이 존재해서 1~6의 값을 가짐
    asdqwe 순으로 블럭 num을 할당함.

---
+++ program/Demo/MapEditor/Assets/Script/BlockSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class BlockSpawn : MonoBehaviour {
+    public GameObject blockPos;
+    public Block[,] blockArray;
+    Block blockClones;
+
+    // Use this for initialization
+    void Awake () {
+        blockArray = new Block[20,20];
+        for (int i = 0; i < 20; i++)
+            for (int j = 0; j < 20; j++)
+            {
+                blockClones = Instantiate(blockPos, new Vector3(1.2f * i, 1.2f * j, 0), blockPos.transform.rotation).GetComponent<Block>();
+                blockArray[i, j] = blockClones;
+            }
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        blockArray[10, 10].GetComponent<Block>().blockNum = 2;
+    }
+}
