7afd32fd17362710b91e8f224ca1d04646652143
psshoon <psshoon123@naver.com>
2018-03-04 23:45:39 +0900

    유니티 물리시스템에 문제발견..
    transform을 이용한 변환때 물리 계산이 빠져서 작동을 안하게되어
    rigidbody를 이용한 모든게 작동을 안함..
    2017.0.3버전부터 적용된 사실이라 이제암..
    다른방법 찾아보는중

--- program/Demo/UnityStudy/Assets/Scripts/Player/PlayerMovement.cs
+++ program/Demo/UnityStudy/Assets/Scripts/Player/PlayerMovement.cs
@@ -16,6 +16,8 @@ public class PlayerMovement : MonoBehaviour {
     int turnDir;
     float turnAngle = 15;
 
+    Vector3 eulerAngleVelocity = new Vector3(0, 100, 0);
+
 
     void Awake()
     {
@@ -26,34 +28,46 @@ public class PlayerMovement : MonoBehaviour {
 
     void FixedUpdate()
     {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        //movePos.Set(h, 0, v);
+        movePos = movePos.normalized * 2f * Time.deltaTime;
+
+        //rigidbody.MovePosition(transform.position + movePos);
+
+        rigidbody.MoveRotation(rigidbody.rotation);
+
         if (avatar)
         {
             if (rigidbody /*&& !avatar.GetBool("StartAttack")*/) // 여기다가 키입력을 두니까 공격도중 속도가 안떨어짐 그래서 일일히넣음...
             {
                 if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
                 {
+                    //print(transform.forward.normalized);
+                    //rigidbody.MovePosition(transform.position + transform.forward * moveSpeed * Time.deltaTime);
                     if (Input.GetKey(KeyCode.A)) //Quaternion.LookRotation(new Vector3(-1, 0, 0))
                         movePos.Set(-1, 0, 1);
 
-                    else if (Input.GetKey(KeyCode.D))
+                    if (Input.GetKey(KeyCode.D))
                         movePos.Set(1, 0, -1);
 
-                    else if(Input.GetKey(KeyCode.W))
+                    if(Input.GetKey(KeyCode.W))
                         movePos.Set(1, 0, 1);
 
-                    else if(Input.GetKey(KeyCode.S))
-                        movePos.Set(-1, 0, 1);
+                    if(Input.GetKey(KeyCode.S))
+                        movePos.Set(-1, 0, -1);
 
-                    else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+                    if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
                         movePos.Set(0, 0, 1);
 
-                    else if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+                    if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
                         movePos.Set(1, 0, 0);
 
-                    else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+                    if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
                         movePos.Set(-1, 0, 0);
 
-                    else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+                    if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
                         movePos.Set(0, 0, -1);
 
                     if (!avatar.GetBool("StartAttack")) // 공격중이 아니며 키를 누르고 있을때.
@@ -61,7 +75,6 @@ public class PlayerMovement : MonoBehaviour {
                         Dash(movePos);
                     }
                 }
-
                 else
                     StopWalking();
 
@@ -80,18 +93,21 @@ public class PlayerMovement : MonoBehaviour {
     {
         Vector3 movePos1 = movePos.normalized * moveSpeed * Time.deltaTime;
 
-        turnDir = Turnjudge(transform.forward, movePos);
-        if (Vector3.Angle(transform.forward, movePos) > turnAngle)
+        turnDir = Turnjudge(transform.forward, movePos.normalized);
+        if (Vector3.Angle(transform.forward, movePos.normalized) > turnAngle)
         {
+            
             transform.Rotate(new Vector3(0, turnDir * turnSpeedTime, 0), Space.World);
+            //rigidbody.AddForce(transform.forward * 60);
         }
 
         else
         {
             transform.rotation = Quaternion.LookRotation(movePos.normalized);
-            rigidbody.MovePosition(transform.position + movePos1);
+            //rigidbody.MovePosition(transform.position + movePos1);
             IsWalking();
         }
+        
     }
 
 
@@ -103,6 +119,11 @@ public class PlayerMovement : MonoBehaviour {
     void StopWalking()
     {
         avatar.SetBool("Dash", false);
+    }
+
+    public void DashEndForce()
+    {
+        //rigidbody.AddForce(transform.forward.normalized * 400);
     }
 
     public int Turnjudge(Vector3 forward, Vector3 dir)
