68d465447aa047e47d355f2dd268ce7dfcb25dca
psshoon <psshoon123@naver.com>
2018-03-07 11:58:38 +0900

    타격1에 이펙트 넣고 위치랑 회전 고침.

--- program/Demo/UnityDemo/Day3-1/Assets/Scripts/Player/PlayerEffect.cs
+++ program/Demo/UnityDemo/Day3-1/Assets/Scripts/Player/PlayerEffect.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class PlayerEffect : MonoBehaviour {
     
     GameObject slash1;
+    public GameObject EffectPos;
 
     public void PlayEffect1()
     {
+        print("123");
         slash1.SetActive(true);
-        slash1.transform.position = transform.position;
+        slash1.transform.position = EffectPos.transform.position;
+        slash1.transform.rotation = EffectPos.transform.rotation * slash1.transform.rotation;
         slash1.GetComponent<ParticleSystem>().Play();
         //slash1.SetActive(false);
     }
 
     // Use this for initialization
     void Awake() {
-        slash1 = Instantiate(Resources.Load("Prefabs/slash1")) as GameObject;
+        slash1 = Instantiate(Resources.Load("Prefabs/slash2")) as GameObject;
         slash1.SetActive(false);
     }
 	
