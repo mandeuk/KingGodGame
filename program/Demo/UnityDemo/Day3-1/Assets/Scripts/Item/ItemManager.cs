2a04383f14ddcb32ce4758ba9ffd1964fa6b554f
mandeuk <lih2677@naver.com>
2018-04-08 14:33:17 +0900

    아이템 추가를 위한 프레임워크 코드, 아이템 프리팹 생성
    ItemManager를 통해 모든 아이템정보를 관리하도록 개발할 예정
    아이템 오브젝트를 Manager에서 생성 및 관리하기 위한 아이템 prefab 추가

---
+++ program/Demo/UnityDemo/Day3-1/Assets/Scripts/Item/ItemManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+    public List<GameObject> itemList = new List<GameObject>(); //아이템을 저장할 리스트 선언
+    public List<GameObject> useditemList = new List<GameObject>(); //맵에 스폰한 아이템을 저장할 리스트 선언
+
+
+    private void Awake()
+    {
+        for (int i = 0; i < 10; ++i)
+        {
+            itemList.Add(GenerateItem());//Item을 생성해서 넣어야 함
+        }
+
+        SpawnItem();
+    }
+    
+
+    public GameObject GenerateItem()
+    {
+        GameObject cloneItem = Instantiate(Resources.Load("Prefabs/Item")) as GameObject;//Items 생성
+
+        cloneItem.SetActive(false);
+
+
+        return cloneItem;
+    }
+    
+    void SpawnItem()
+    {
+        useditemList.Add(itemList[0]);//지도에 스폰된 아이템을 useditemList에 추가
+
+        //Instantiate(itemList[0]);
+        Vector3 vec;
+        vec.x = 100;
+        vec.y = 100;
+        vec.z = 100;
+        Quaternion qua;
+        qua.x = 0;
+        qua.y = 0;
+        qua.z = 0;
+        qua.w = 1;
+        //itemList[0].transform.SetPositionAndRotation(vec, qua);
+        itemList[0].transform.SetPositionAndRotation(new Vector3(10,1,10),new Quaternion(0,0,0,1));
+        //itemList[0].transform.position = EnemyDiePosition;
+
+        itemList[0].SetActive(true);
+
+        itemList.Remove(itemList[0]);//지도에 스폰된 아이템을 itemList에서 삭제
+    }
+    
+    
+    
+}
