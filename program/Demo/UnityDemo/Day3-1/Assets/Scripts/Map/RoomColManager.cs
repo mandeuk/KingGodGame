d207a7d9333c52949d020d26fc830d85aa74a010
psshoon <psshoon123@naver.com>
2018-04-15 17:39:57 +0900

    방의 상태에따른 갱신 추가.
    1. 방의 상태를 3가지로 나누어 플레이어 및 몬스터와 상호작용 할 수 있게끔 나눔
    2. cs파일 분할 및 적용되는 순서,  구조 고려하여 확실하게 제작
    3. 맵과 콜라이더 프리펩 수정
    4. 룸이 생성되는 구조를 기존의 구조에서  mesh가 있는 프리펩에 생성되도록 바꿈  -> 조금더 유기적으로 컴포넌트들이 연결됨.

---
+++ program/Demo/UnityDemo/Day3-1/Assets/Scripts/Map/RoomColManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomColManager : MonoBehaviour {
+    public GameObject leftWallCol;
+    public GameObject rightWallCol;
+    public GameObject frontWallCol;
+    public GameObject backWallCol;
+
+    public GameObject leftDoorCol;
+    public GameObject rightDoorCol;
+    public GameObject frontDoorCol;
+    public GameObject backDoorCol;
+
+    void Awake () {
+        
+    }
+
+    public void LeftDoorOpen()
+    {
+        leftWallCol.SetActive(false);
+    }
+    public void RightDoorOpen()
+    {
+        rightWallCol.SetActive(false);
+    }
+    public void FrontDoorOpen()
+    {
+        frontWallCol.SetActive(false);
+    }
+    public void BackDoorOpen()
+    {
+        backWallCol.SetActive(false);
+    }
+
+
+}
