52f7aa778484d9aa09daa9e146b07dd822942e5f
mandeuk <lih2677@naver.com>
2018-05-09 04:08:41 +0900

    로딩화면, 인트로화면, 게임플레이 UI관련 스크립트코드와 씬 추가
    인트로화면에서 영상재생이 가능하며 시작버튼 누를 시 Fade In/Out효과가 나타나며 로딩씬과 게임플레이씬으로 넘어간다.

---
+++ program/Demo/UnityDemo/Day3-1/Assets/Scripts/UI/IntroVideoManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroVideoManager : MonoBehaviour {
+    UnityEngine.Video.VideoPlayer introvideo;
+
+    public GameObject logoimage;
+    public GameObject gamestartBtn;
+
+    bool videohasplayed;
+    // Use this for initialization
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start () {
+        introvideo = gameObject.GetComponent<UnityEngine.Video.VideoPlayer>();
+        videohasplayed = false;
+        logoimage.SetActive(false);
+        gamestartBtn.SetActive(false);
+        introvideo.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(introvideo.isPlaying)
+        {
+            videohasplayed = true;
+        }
+        else
+        {
+            StartCoroutine(DisableVideo());
+        }
+	}
+
+    IEnumerator DisableVideo()
+    {
+        if ((introvideo.isPlaying == false) && videohasplayed)
+        {
+            logoimage.SetActive(true);//로고이미지 띄우기
+            gamestartBtn.SetActive(true);//플레이버튼 띄우기
+            gameObject.SetActive(false);//동영상 안보이게하기
+            yield break;
+        }
+    }
+}
