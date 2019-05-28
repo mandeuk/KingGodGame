59510de7e6e0ea073a84815a8dc67a480b31f244
psshoon <psshoon123@naver.com>
2019-05-28 15:52:46 +0900

    정리중
    기존파일 삭제하고 다시올림
    이제 커밋도 같이할예정

---
+++ program/Day3-1/Assets/Scripts/StormCloudPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormCloudPosition : MonoBehaviour {
+    public static StormCloudPosition instance = null;
+    
+	void Awake () {
+        instance = this;
+    }
+}
